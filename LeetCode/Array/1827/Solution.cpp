ecb9ed1bae6b5073c9205ff9a78783a58ccb8166
CnLzh <cnlzh1516@outlook.com>
2022-12-13 22:00:57 +0800

    Update 1832 Solution.cpp

--- LeetCode/Array/1827/Solution.cpp
+++ LeetCode/Array/1827/Solution.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
-    int minOperations(vector<int>& nums) {
+    int minOperations(vector<int> &nums) {
         int add_num = 0;
-        for(size_t i = 1; i < nums.size(); i++){
-            if(nums[i]  <= nums[i-1]){
-                add_num += nums[i-1] - nums[i] + 1;
-                nums[i] = nums[i-1] + 1;
+        for (size_t i = 1; i < nums.size(); i++) {
+            if (nums[i] <= nums[i - 1]) {
+                add_num += nums[i - 1] - nums[i] + 1;
+                nums[i] = nums[i - 1] + 1;
             }
         }
         return add_num;
