4d8c858f0786491e80fd27f7e2efa1c9bc8856fc
CnLzh <cnlzh1516@outlook.com>
2022-12-14 23:58:22 +0800

    Update Solution

--- LeetCode/Array/1781/Solution.cpp
+++ LeetCode/Array/1781/Solution.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-	int beautySum(string s) {
-		int res = 0;
-		for (int i = 0; i < s.size(); i++) {
-			int v[26] = { 0 };
-			for (int j = i; j < s.size(); j++) {
-				v[s[j] - 'a']++;
-				int maxnum = 0, minnum = s.size();
-				for (int k = 0; k < 26; k++) {
-					if (v[k] > 0) {
-						maxnum = max(maxnum, v[k]);
-						minnum = min(minnum, v[k]);
-					}
-				}
-				res = res + maxnum - minnum;
-			}
-		}
+    int beautySum(string s) {
+        int res = 0;
+        for (int i = 0; i < s.size(); i++) {
+            int v[26] = {0};
+            for (int j = i; j < s.size(); j++) {
+                v[s[j] - 'a']++;
+                int maxnum = 0, minnum = s.size();
+                for (int k = 0; k < 26; k++) {
+                    if (v[k] > 0) {
+                        maxnum = max(maxnum, v[k]);
+                        minnum = min(minnum, v[k]);
+                    }
+                }
+                res = res + maxnum - minnum;
+            }
+        }
         return res;
-	}
+    }
 };
--- LeetCode/Array/1832/Solution.cpp
+++ LeetCode/Array/1832/Solution.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool checkIfPangram(string sentence) {
         int p = 0;
-        for (auto it : sentence) {
+        for (auto it: sentence) {
             p |= 1 << (it - 'a');
         }
         return p == (1 << 26) - 1;
