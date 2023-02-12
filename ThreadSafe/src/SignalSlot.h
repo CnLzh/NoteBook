21b640d988e0f2aa6b514df190e9ee403d0e86f5
CnLzh <cnlzh1516@outlook.com>
2023-02-12 22:03:42 +0800

    Update Thread Safe src

---
+++ ThreadSafe/src/SignalSlot.h
@@ -0,0 +1,9 @@
+// Copyright (c) 2023
+// File:    SignalSlot.h
+// Brief:   Definition the SignalImpl, Signal, Slot class.
+// Author:  cnlzh
+
+#ifndef SRC_SIGNAL_SLOT_H_
+#define SRC_SIGNAL_SLOT_H_
+
+#endif //SRC_SIGNAL_SLOT_H_
