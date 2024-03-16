4e3496ed7bf8db93a301c8e37d0c83bcc1f45165
echo <62585551+CnLzh@users.noreply.github.com>
2024-03-16 18:48:15 +0800

    docs: Update MagicCode.

--- MagicCode/README.md
+++ MagicCode/README.md
@@ -1,7 +1,9 @@
 # 黑魔法代码
 
-一些神奇的代码记录。  :magic_wand:
+记录一些有趣或常用的代码片段。  :magic_wand:
 
 ## 目录
 
-[翻转8字节](https://github.com/CnLzh/NoteBook/tree/main/MagicCode/src/ReverseUint32.cc)
+[宏定义](https://github.com/CnLzh/NoteBook/tree/main/MagicCode/src/public_define.h)
+
+[翻转8字节](https://github.com/CnLzh/NoteBook/tree/main/MagicCode/src/reverse_uint32.cc)
---
+++ MagicCode/src/public_define.h
@@ -0,0 +1,71 @@
+#include <cassert>
+
+#define DISALLOW_COPY_AND_ASSIGN(ClassName) \
+  ClassName(const ClassName&) = delete;     \
+  void operator=(const ClassName&) = delete;
+
+#define DISALLOW_CONSTRUCTORS_AND_DESTRUCTORS(ClassName) \
+  ClassName() = delete;                                  \
+  ~ClassName() = delete;
+
+#define INSTANCE_CLASS(ClassName)   \
+ public:                            \
+  static ClassName& GetInstance() { \
+    static ClassName instance;      \
+    return instance;                \
+  }                                 \
+                                    \
+ private:                           \
+  ClassName() = default;
+
+#define ASSERT_NULLPTR(...)                     \
+  do {                                          \
+    __assert_nullptr_impl_(##__VA_ARGS__);        \
+  } while (0)
+
+#define DELETE_MEMORY(...)                      \
+  do {                                          \
+    __delete_memory_impl_(##__VA_ARGS__);         \
+  } while (0)
+
+#define MALLOC_MEMORY(len, ...)                 \
+  do {                                          \
+    assert(len > 0);                            \
+    __malloc_memory_impl_(len, ##__VA_ARGS__);    \
+  } while (0)
+
+template <typename T>
+inline void __assert_nullptr_impl_(T&& arg) {
+	assert(std::forward<T>(arg) != nullptr);
+}
+
+template <typename T, typename... Args>
+inline void __assert_nullptr_impl_(T&& arg, Args&&... args) {
+	__assert_nullptr_impl_(std::forward<T>(arg));
+	__assert_nullptr_impl_(std::forward<Args>(args)...);
+}
+
+template <typename T>
+inline void __delete_memory_impl_(T*& arg) {
+	if (arg != nullptr) {
+		delete arg;
+		arg = nullptr;
+	}
+}
+
+template <typename T, typename... Args>
+inline void __delete_memory_impl_(T*& arg, Args&&... args) {
+	__delete_memory_impl_(arg);
+	__delete_memory_impl_(std::forward<Args>(args)...);
+}
+
+template <typename T>
+inline void __malloc_memory_impl_(size_t len, T*& arg) {
+	arg = new T(len);
+}
+
+template <typename T, typename... Args>
+inline void __malloc_memory_impl_(size_t len, T*& arg, Args&&... args) {
+	__malloc_memory_impl_(len, arg);
+	__malloc_memory_impl_(len, std::forward<Args>(args)...);
+}
