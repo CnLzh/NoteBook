c717a98e72fd46ab86c1ca3a5a4c4e0fb6e8fd31
echo <62585551+CnLzh@users.noreply.github.com>
2024-03-19 14:45:07 +0800

    fix: some error.

--- MagicCode/src/public_define.h
+++ MagicCode/src/public_define.h
@@ -18,54 +18,54 @@
  private:                           \
   ClassName() = default;
 
-#define ASSERT_NULLPTR(...)                     \
-  do {                                          \
-    __assert_nullptr_impl_(##__VA_ARGS__);        \
+#define ASSERT_NULLPTR(...)                \
+  do {                                     \
+    __assert_nullptr_impl_(##__VA_ARGS__); \
   } while (0)
 
-#define DELETE_MEMORY(...)                      \
-  do {                                          \
-    __delete_memory_impl_(##__VA_ARGS__);         \
+#define DELETE_ARRAY(...)                \
+  do {                                   \
+    __delete_array_impl_(##__VA_ARGS__); \
   } while (0)
 
-#define MALLOC_MEMORY(len, ...)                 \
-  do {                                          \
-    assert(len > 0);                            \
-    __malloc_memory_impl_(len, ##__VA_ARGS__);    \
+#define MALLOC_ARRAY(len, ...)                \
+  do {                                        \
+    assert(len > 0);                          \
+    __malloc_array_impl_(len, ##__VA_ARGS__); \
   } while (0)
 
 template <typename T>
 inline void __assert_nullptr_impl_(T&& arg) {
-	assert(std::forward<T>(arg) != nullptr);
+  assert(std::forward<T>(arg) != nullptr);
 }
 
 template <typename T, typename... Args>
 inline void __assert_nullptr_impl_(T&& arg, Args&&... args) {
-	__assert_nullptr_impl_(std::forward<T>(arg));
-	__assert_nullptr_impl_(std::forward<Args>(args)...);
+  __assert_nullptr_impl_(std::forward<T>(arg));
+  __assert_nullptr_impl_(std::forward<Args>(args)...);
 }
 
 template <typename T>
-inline void __delete_memory_impl_(T*& arg) {
-	if (arg != nullptr) {
-		delete arg;
-		arg = nullptr;
-	}
+inline void __delete_array_impl_(T*& arg) {
+  if (arg != nullptr) {
+    delete[] arg;
+    arg = nullptr;
+  }
 }
 
 template <typename T, typename... Args>
-inline void __delete_memory_impl_(T*& arg, Args&&... args) {
-	__delete_memory_impl_(arg);
-	__delete_memory_impl_(std::forward<Args>(args)...);
+inline void __delete_array_impl_(T*& arg, Args&&... args) {
+  __delete_array_impl_(arg);
+  __delete_array_impl_(std::forward<Args>(args)...);
 }
 
 template <typename T>
-inline void __malloc_memory_impl_(size_t len, T*& arg) {
-	arg = new T(len);
+inline void __malloc_array_impl_(size_t len, T*& arg) {
+  arg = new T[len];
 }
 
 template <typename T, typename... Args>
-inline void __malloc_memory_impl_(size_t len, T*& arg, Args&&... args) {
-	__malloc_memory_impl_(len, arg);
-	__malloc_memory_impl_(len, std::forward<Args>(args)...);
+inline void __malloc_array_impl_(size_t len, T*& arg, Args&&... args) {
+  __malloc_array_impl_(len, arg);
+  __malloc_array_impl_(len, std::forward<Args>(args)...);
 }
